eb0e4b2c1b347c3f8cba073bfdf0aa165db822ec
Mustafa Paksoy <paksoy@gmail.com>
2011-12-22 15:55:13 +0200

    Create unit test file for rules.

---
+++ test/unit/RuleTest.java
@@ -0,0 +1,12 @@
+package unit;
+
+import play.test.*;
+import org.junit.*;
+ 
+public class RuleTest extends UnitTest {
+    @Test
+    public void aTest() {
+        assertEquals(2, 1 + 1); // A really important thing to test
+    }
+}
+
