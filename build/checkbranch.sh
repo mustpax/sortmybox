e6f468818fccfd4f45a1759a5ceac058533990b9
Mustafa Paksoy <paksoy@gmail.com>
2012-04-19 01:39:42 -0700

    Ensure that the branch is clean before deploys

--- build/checkbranch.sh
+++ build/checkbranch.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 # @author mustpax
-# Display error message and exit with status code 1 if current git branch does
-# not match first argument. Otherwise silently exit with status code 0.
+# First ensure that the branch is clean.
+# Then check that the current branch matches the first argument to script, if
+# not exit with error.
+
+clean=$(git status | grep "nothing to commit (working directory clean)")
+if [ -z "$clean" ]; then
+    echo Error: There are uncommitted changes
+    exit 1
+else
+    echo Branch is clean
+fi
 
 branch=$(git symbolic-ref -q HEAD | awk -F/ '{print $NF}')
 if [ "$1" = "$branch" ]; then
