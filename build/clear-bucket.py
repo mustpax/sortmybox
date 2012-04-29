729154330619e4824db4c683474e56818031fc8a
Mustafa Paksoy <paksoy@gmail.com>
2012-04-29 15:24:46 -0700

    Make clear bucket script safer.

--- build/clear-bucket.py
+++ build/clear-bucket.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
-def main():
+# Delete all files in the static.sortmybox.com S3 bucket
+
+def main(args):
+    if (len(args) == 1) or (args[1] != '-f'):
+        print "To delete all files in bucket type: ./clear-bucket -f"
+        return
     import boto
     conn = boto.connect_s3()
     bucket = conn.get_bucket('static.sortmybox.com')
-    print bucket.list()
+    bucket.delete_keys(list(bucket.list()))
 
 if __name__ == '__main__':
-    main()
+    import sys
+    main(sys.argv)
