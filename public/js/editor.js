b4a3de7b3c6b4218e50dba7f620843011c8ae1ef
Mustafa Paksoy <paksoy@gmail.com>
2012-05-19 19:28:04 -0700

    Extract file explorer into jQuery module.

--- public/js/editor.js
+++ public/js/editor.js
@@ -19,57 +19,6 @@
         return ret;
     };
 
-    var dirCache = {};
-    var curReq = null; 
-    /**
-     * Get list of directories inside given directory from
-     * the server.
-     * @param optional path of directory to get a listing of
-     * @param cb function is called with the output
-     */
-    function getDirs(path, cb) {
-        if (curReq) {
-            // There is already a request for this
-            // path in flight ignore this one
-            if (curReq.path === path) {
-                console.log('already requesting', path);
-                return;
-            }
-
-            // There is a request for another path abort it
-            console.log('request obsolete, aborting', curReq.path);
-            curReq.req.abort();
-            curReq = null;
-        }
-
-        if (dirCache[path]) {
-            console.log('cache hit', path);
-            cb(dirCache[path]);
-            return;
-        }
-
-        console.log('cache miss, request from server', path);
-        var req = $.ajax({
-            type: 'GET',
-            url: '/dirs',
-            data: {
-                'path': path || '/',
-                'authenticityToken' : window.csrfToken
-            },
-            success: function(dirs) {
-                curReq = null;
-                dirCache[path] = dirs;
-                cb(dirs);
-            },
-            error: function() {
-                cb();
-            }
-        });
-        
-        curReq = { path : path,
-                   req  : req };
-    };
-    
     var templateCache = {};
     /**
      * Render the template with the given name into a jQuery
@@ -89,79 +38,8 @@
 
         return $(t(context));
     };
+    sortbox.template = template;
     
-
-    function basename(path) {
-        return _.chain(path.split('/')).filter(function(x) {
-		                                    return !!x;
-		                                })
-		                               .last()
-		                               .value();
-    };
-
-    function displayDirs(path, dirs, cell, isLoading) {
-        var pathList = _.filter(path.split('/'), function(x) { return !!x; });
-        
-        var exp = $(cell).find('.exp');
-        if (! exp.length) {
-            exp = $('<ul class="exp nav nav-list">');
-            $(cell).find('.dest-wrap').append(exp);
-        }
-        
-        exp.empty();
-        
-        function select() {
-            return template('exp-select', { path: basename(path),
-				                            dataPath: path });
-        };
-
-        function upLink() {
-            var upPath = '/'  + pathList.slice(0, pathList.length - 1).join('/');
-            return template('exp-uplink', {dataPath : upPath});
-        };
-        
-        // Add up link if not top dir
-        if (! _.isEmpty(pathList)) {
-            exp.append(select());
-            exp.append(upLink());
-        }
-        
-        function loading() {
-            return template('exp-loading');
-        }
-        
-        if (isLoading) {
-            exp.append(loading());
-        } else {
-            if (_.isUndefined(dirs)){
-                exp.append($("<li><a><em>Can't list folder contents</em></a></li>"));
-            } else {
-	            dirs.sort();
-	            if (_.isEmpty(dirs)) {
-	                exp.append($("<li><a><em>No more folders</em></a></li>"));
-	            } else {
-		            $.each(dirs, function(i, v) {
-		                exp.append(template('exp-folder', {
-		                    path: basename(v),
-		                    dataPath: v
-		                }));
-		            });
-	            }
-            }
-        }
-    };
-    
-    function dirUpdater() {
-        var cell = $(this).parents('td').first();
-        var path = $(this).val() || '/';
-        displayDirs(path, null, cell, true);
-        getDirs(path, function(dirs) {
-            displayDirs(path, dirs, cell);
-        });
-    };
-    
-    $('.rule .dest').live('focus change', dirUpdater);
-    $('.rule .dest').live('keyup', _.debounce(dirUpdater, 1500));
 
     $('.rule .dest').live('focus', function() {
         var cell = $(this).parents('td').first();
@@ -323,5 +201,8 @@
 
         $('.alert-created').slideDown('slow');
     });
-
+    
+    $(function() {
+        $('.rule .dest').explorer();
+    });
 })(window, jQuery);
