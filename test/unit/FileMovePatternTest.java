b7a830a0fa1545f49b8c5ddace28e1f53f167b37
Mustafa Paksoy <paksoy@gmail.com>
2012-05-26 16:41:15 -0700

    Create isValidFilename() util.

--- test/unit/FileMovePatternTest.java
+++ test/unit/FileMovePatternTest.java
@@ -15,13 +15,13 @@ public class FileMovePatternTest extends UnitTest {
         String[] bad = { "foo:", "foo\\", "bar*", "   ?", "sd<", ">", "\"", "|"};
         String[] good = {"/a / b $#@", "a"};
         for (String str: bad) {
-            assertTrue("Filename should be bad but isn't: " + str,
-                        Dropbox.DISALLOWED_FILENAME_CHARS.matcher(str).find());
+            assertFalse("Filename should be bad but isn't: " + str,
+                        Dropbox.isValidFilename(str));
         }
         
         for (String str: good) {
-            assertFalse("Filename should be good but isn't: " + str,
-                        Dropbox.DISALLOWED_FILENAME_CHARS.matcher(str).find());
+            assertTrue("Filename should be good but isn't: " + str,
+                        Dropbox.isValidFilename(str));
         }
     }
 
