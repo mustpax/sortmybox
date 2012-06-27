730fbf3ede8b9b3aef765c6dac01b0c380d82335
Mustafa Paksoy <paksoy@gmail.com>
2012-06-27 00:22:07 -0700

    Create static URLs.

--- app/box/Box.java
+++ app/box/Box.java
@@ -13,8 +13,17 @@ public class Box {
     public static final String API_KEY = Play.mode.isProd() ? Play.configuration.getProperty("box.apiKey") :
                                                               Play.configuration.getProperty("box.apiKeyDev");
 
+    
+    public static class URLs {
+        public static final String BASE_V1 = "https://www.box.com/api/1.0";
+        public static final String BASE_V1_REST = "https://www.box.com/api/1.0/rest";
+        public static final String BASE_V1_AUTH = "https://www.box.com/api/1.0/auth";
+
+        public static final String BASE_V2 = "https://api.box.com/2.0";
+    }
+    
     public static String getTicket() {
-        WSRequest ws = WS.url("https://www.box.com/api/1.0/rest")
+        WSRequest ws = WS.url(URLs.BASE_V1_REST)
                          .setParameter("action", "get_ticket")
                          .setParameter("api_key", API_KEY);
         HttpResponse resp = ws.get();
@@ -27,14 +36,14 @@ public class Box {
     }
 
     public static String getAuthUrl(String ticket) {
-        return "https://www.box.com/api/1.0/auth/" + WS.encode(ticket);
+        return URLs.BASE_V1_AUTH + "/" + WS.encode(ticket);
     }
 
     public static BoxAccount getAccount(String ticket) {
-        WSRequest ws = WS.url("https://www.box.com/api/1.0/rest")
-                .setParameter("action", "get_auth_token")
-                .setParameter("api_key", API_KEY)
-                .setParameter("ticket", ticket);
+        WSRequest ws = WS.url(URLs.BASE_V1_REST)
+                         .setParameter("action", "get_auth_token")
+                         .setParameter("api_key", API_KEY)
+                         .setParameter("ticket", ticket);
 
         HttpResponse resp = ws.get();
         if (resp.success()) {
