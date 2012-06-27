e385c6e8e6f552d07b24635048137e10110f549e
Mustafa Paksoy <paksoy@gmail.com>
2012-06-26 22:31:17 -0700

    ApiClientFactory for generation ApiClient's

---
+++ app/common/api/ApiClientFactory.java
@@ -0,0 +1,17 @@
+package common.api;
+
+import models.User;
+import box.BoxClientFactory;
+import dropbox.client.DropboxClientFactory;
+
+public class ApiClientFactory {
+    public static ApiClient create(User u) {
+        switch (u.accountType) {
+        case BOX:
+            return BoxClientFactory.create(u);
+        case DROPBOX:
+            return DropboxClientFactory.create(u);
+        }
+        throw new IllegalArgumentException("Unrecognized account type: " + u.accountType);
+    }
+}
