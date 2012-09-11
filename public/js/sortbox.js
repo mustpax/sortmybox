f85b093d0da4097f96297e6a39765728ba9b4375
Mustafa Paksoy <paksoy@gmail.com>
2012-09-10 19:35:13 -0700

    Declare console methods to prevent errors.

--- public/js/sortbox.js
+++ public/js/sortbox.js
@@ -1,12 +1,48 @@
+// ========= Begin code from HTML5 Boilerplate
+// Modified by Mustafa Paksoy, 2012
+// Copyright (c) HTML5 Boilerplate
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    'use strict';
+
+    _.templateSettings.escape = /\{\{(.+?)\}\}/g;
+    window.sortbox = {};
+
+    if (!(window.console && window.console.log)) {
+        var noop = function() {};
+        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+        var length = methods.length;
+        var console = window.console = {};
+        while (length--) {
+            console[methods[length]] = noop;
+        }
+    }
+}());
+// ========= End code from HTML5 Boilerplate
+
 (function($) {
     'use strict';
 
-    var templateCache = {},
-        sortbox = {};
-
-    window.sortbox = sortbox;
-    _.templateSettings.escape = /\{\{(.+?)\}\}/g;
-
+    var templateCache = {};
     /**
      * Render the template with the given name into a jQuery
      * wrapped unattached DOM element.
@@ -25,7 +61,7 @@
 
         return $(t(context));
     }
-    sortbox.template = template;
+    window.sortbox.template = template;
 
     $('.dropdown-toggle').dropdown();
     $('.easter').live('click', function() {
