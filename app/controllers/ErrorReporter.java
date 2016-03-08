9bf7bcade19067a577b3b183af4ae8c28ea946eb
Mustafa Paksoy <paksoy@gmail.com>
2016-03-07 20:16:39 -0800

    Submit stack traces to Sentry. Create task queue for Sentry.

--- app/controllers/ErrorReporter.java
+++ app/controllers/ErrorReporter.java
@@ -2,9 +2,20 @@ package controllers;
 
 import models.User;
 import notifiers.Mails;
+import play.Play;
 import play.mvc.Catch;
 import play.mvc.Controller;
+import play.mvc.Http.Header;
 
+import java.util.Map;
+
+import com.getsentry.raven.Raven;
+import com.getsentry.raven.RavenFactory;
+import com.getsentry.raven.appengine.AppEngineRavenFactory;
+import com.getsentry.raven.appengine.event.helper.AppEngineEventBuilderHelper;
+import com.getsentry.raven.event.Event;
+import com.getsentry.raven.event.EventBuilder;
+import com.getsentry.raven.event.interfaces.ExceptionInterface;
 import com.google.appengine.api.datastore.Key;
 
 /**
@@ -13,6 +24,31 @@ import com.google.appengine.api.datastore.Key;
  * @author mustpax
  */
 public class ErrorReporter extends Controller {
+    private static final String DSN = Play.configuration.getProperty("raven.dsn");
+
+    private static final Raven RAVEN = AppEngineRavenFactory.ravenInstance(DSN);
+
+    private static void logSentry(Throwable e, User u) {
+        EventBuilder eb = new EventBuilder().withMessage(e.getMessage())
+                .withLevel(Event.Level.ERROR)
+                .withSentryInterface(new ExceptionInterface(e));
+        
+        if (u != null) {
+            eb = eb.withTag("user", u.getKey().toString());
+        }
+        if (request.url != null) {
+            eb = eb.withTag("url", request.url);
+        }
+        if (request.remoteAddress != null) {
+            eb = eb.withTag("ip", request.remoteAddress);
+        }
+        for (Header header: request.headers.values()) {
+            eb = eb.withTag("Header: " + header.name, header.value());
+        }
+
+        RAVEN.sendEvent(eb.build());
+    }
+
     @Catch(Exception.class)
     static void logError(Throwable e) {
         User u = Login.getUser();
