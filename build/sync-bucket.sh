a5c0470b252b0757c6e55d9c01958505b21d864d
Mustafa Paksoy <paksoy@gmail.com>
2016-03-10 19:04:32 -0800

    Adapt bucket sync script to modules.

--- build/sync-bucket.sh
+++ build/sync-bucket.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Sync contents of the static file directory with S3
-ver=$(grep version war/WEB-INF/appengine-web.xml | cut -d">" -f2 | cut -d"<" -f1)
+ver=$(grep version war/default/WEB-INF/appengine-web.xml | cut -d">" -f2 | cut -d"<" -f1)
 echo Uploading to S3 with prefix: $ver
 build/s3put -e "$ver" -p `pwd` -b static.sortmybox.com public/
 
