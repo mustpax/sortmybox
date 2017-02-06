24581ea87db12d850d857197074225e1249e16fe
Moose Paksoy <paksoy@gmail.com>
2017-02-06 09:35:25 -0800

    Add bootstrap.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.render('index');
 });
 
 var port = process.env.PORT || 3000;
