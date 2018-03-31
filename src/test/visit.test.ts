c107513ea2b56bda1cf59cdccf53001195e5e98a
Mustafa Paksoy <paksoy@gmail.com>
2018-03-31 12:15:15 -0700

    Move tests under src

---
+++ src/test/visit.test.ts
@@ -0,0 +1,15 @@
+import { VisitService as VS } from '../models';
+
+describe('Visit', function() {
+  it("save", async function(done) {
+    expect(true).toBe(true);
+    done();
+    // let dates = [1522522000482, 1522522060482];
+    // let visits = dates.map(date => {
+    //   let visit = VS.makeNew();
+    //   visit.created = new Date(date);
+    //   return visit;
+    // });
+    // let ids = await VS.save(visits);
+  });
+});
