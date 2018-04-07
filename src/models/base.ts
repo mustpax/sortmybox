eff65b4e13a6643077a51daf37d02ac4f719b6a6
Mustafa Paksoy <paksoy@gmail.com>
2018-04-06 20:26:42 -0700

    Create abstract model class and move logic over to it

--- src/models/base.ts
+++ src/models/base.ts
@@ -1,6 +1,7 @@
 import { Query } from '@google-cloud/datastore/query';
 import { DatastoreKey } from '@google-cloud/datastore/entity';
 import joi = require('joi');
+import datastore from './datastore';
 
 export function toArraySchema(schema: joi.SchemaMap): joi.Schema {
   // Add optional id field to every schema
@@ -17,11 +18,14 @@ export interface Model<K> {
   id?: K;
 }
 
-export interface ModelService<K, T extends Model<K>> {
+export interface Schema {
   schema: {
     [key: string]: joi.Schema
   };
   kind: string;
+}
+
+export interface ModelService<K, T extends Model<K>> extends Schema {
   makeNew(): T;
   findByIds(ids: K[]): Promise<T[]>;
   fromEntity(e: object): T;
@@ -34,4 +38,68 @@ export interface ModelService<K, T extends Model<K>> {
   remove(t: T[]): Promise<void>;
   save(t: T[]): Promise<K[]>;
   validate(ts: T[]): joi.ValidationError;
+}
+
+export abstract class AbstractModelService<K, T extends Model<K>> implements ModelService<K, T> {
+  abstract schema: {
+    [key: string]: joi.Schema
+  };
+  abstract kind: string;
+  abstract makeNew(): T;
+  abstract fromEntity(e: object): T;
+  abstract toEntity(t: T): object;
+  abstract keyFromId(id: K): DatastoreKey;
+  abstract idFromKey(key: DatastoreKey): K|undefined;
+
+  async findByIds(ids: K[]) {
+    let keys = ids.map(id => this.keyFromId(id));
+    // datastore.get() returns [results]
+    let [entities] = await datastore.get(keys);
+    return entities.map(e => this.fromEntity(e));
+  }
+
+
+  async query(q: Query) {
+    let resp = await datastore.runQuery(q);
+    let results = resp[0];
+    return results.map(e => this.fromEntity(e));
+  }
+
+  all(): Query {
+    return datastore.createQuery(this.kind);
+  }
+
+  async removeById(ids: (K|undefined)[]) {
+    let keys: DatastoreKey[] = [];
+    for (let id of ids) {
+      if (id) {
+        keys.push(this.keyFromId(id));
+      }
+    }
+    await datastore.delete(keys);
+  }
+
+  // TODO
+  async remove(tarr: T[]): Promise<void> {
+    await this.removeById(tarr.map(t => t.id));
+  }
+
+  async save(tarr: T[]): Promise<K[]> {
+    let error = this.validate(tarr);
+    if (error) {
+      throw error;
+    }
+    let entities = tarr.map(t => this.toEntity(t));
+    let savedEntities = await datastore.save(entities);
+    let mutationResults = savedEntities[0].mutationResults as any[];
+    // mr.key is only set (i.e. non-empty) if datastore generated a key for
+    // us. If we specify a key/id ahead of time, then it's empty.
+    return mutationResults.map(mr => mr.key && mr.key.path[0].id);
+  }
+
+
+  validate(ts: T[]): joi.ValidationError {
+    return joi.validate(ts, toArraySchema(this.schema)).error;
+  }
+
 }
--- src/models/visit.ts
+++ src/models/visit.ts
@@ -1,6 +1,5 @@
-import { Entity, Model, ModelService, toArraySchema } from './base';
+import { Entity, Model, AbstractModelService } from './base';
 import { DatastoreKey } from '@google-cloud/datastore/entity';
-import { Query } from '@google-cloud/datastore/query';
 
 import datastore from './datastore';
 
@@ -11,7 +10,7 @@ export class Visit implements Model<string> {
   created?: Date;
 }
 
-export class VisitSchema implements ModelService<string, Visit> {
+export class VisitSchema extends AbstractModelService<string, Visit> {
   schema = {
     created: joi.date().required()
   };
@@ -21,13 +20,6 @@ export class VisitSchema implements ModelService<string, Visit> {
     let ret = new Visit();
     ret.created = new Date();
     return ret;
-  }
-
-  async findByIds(ids: string[]) {
-    let keys = ids.map(id => this.keyFromId(id));
-    // datastore.get() returns [results]
-    let [entities] = await datastore.get(keys);
-    return entities.map(e => this.fromEntity(e));
   }
 
   toEntity(v: Visit): Entity {
@@ -63,47 +55,6 @@ export class VisitSchema implements ModelService<string, Visit> {
       return undefined;
     }
     return String(k.path[1]);
-  }
-
-  all(): Query {
-    return datastore.createQuery(this.kind);
-  }
-
-  async query(q: Query) {
-    let resp = await datastore.runQuery(q);
-    let results = resp[0];
-    return results.map(e => this.fromEntity(e));
-  }
-
-  async removeById(ids: (string|undefined)[]) {
-    let keys: DatastoreKey[] = [];
-    for (let id of ids) {
-      if (id) {
-        keys.push(datastore.key([this.kind, parseInt(id as string)]));
-      }
-    }
-    await datastore.delete(keys);
-  }
-
-  async remove(visits: Visit[]) {
-    await this.removeById(visits.map(visit => visit.id));
-  }
-
-  async save(visits: Visit[]) {
-    let error = this.validate(visits);
-    if (error) {
-      throw error;
-    }
-    let entities = visits.map(visit => this.toEntity(visit));
-    let savedEntities = await datastore.save(entities);
-    let mutationResults = savedEntities[0].mutationResults as any[];
-    // mr.key is only set (i.e. non-empty) if datastore generated a key for
-    // us. If we specify a key/id ahead of time, then it's empty.
-    return mutationResults.map(mr => mr.key && mr.key.path[0].id);
-  }
-
-  validate(visits: Visit[]): joi.ValidationError {
-    return joi.validate(visits, toArraySchema(this.schema)).error;
   }
 }
 
