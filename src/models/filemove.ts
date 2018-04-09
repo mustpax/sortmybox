011077cb21e6de1856bf5a91ca51b9799bd120a1
Mustafa Paksoy <paksoy@gmail.com>
2018-04-09 14:26:37 -0700

    File move model sans tests

---
+++ src/models/filemove.ts
@@ -0,0 +1,104 @@
+import { Entity, Model, AbstractModelService, DatastoreUtil as dutil } from './base';
+import { DatastoreKey } from '@google-cloud/datastore/entity';
+import { UserService } from './user';
+
+import datastore from './datastore';
+
+import joi = require('joi');
+
+export class FileMove implements Model<FileMoveKey> {
+  id?: FileMoveKey;
+  fromFile?: string;
+  toDir?: string;
+  when?: Date;
+}
+
+export class FileMoveKey {
+  ownerId?: string;
+  fileMoveId?: string;
+}
+
+export class FileMoveSchema extends AbstractModelService<FileMoveKey, FileMove> {
+  schema = {
+    fromFile: joi.string().required(),
+    toDir: joi.string().required(),
+    when: joi.date().required(),
+    hasCollision: joi.boolean().required(),
+    resolvedName: joi.string()
+  };
+  kind = 'FileMove';
+
+  makeNew(ownerId: string): FileMove {
+    let id = new FileMoveKey();
+    id.ownerId = ownerId;
+    let ret: FileMove = {
+      when: new Date(),
+      id
+    };
+    return ret;
+  }
+
+  toEntity(r: FileMove): Entity {
+    if ((! r.id) || (! r.id.ownerId)) {
+      throw new Error('Cannot convert filemove to entity. Missing ownerId ' + r);
+    }
+    return super.toEntity(r);
+  }
+
+  keyFromId(id?: FileMoveKey) {
+    if (!id || !id.ownerId) {
+      throw new Error('Cannot generate key for FileMove with no owner id. ' + this);
+    }
+    let userKey = UserService.keyFromId(id.ownerId);
+    let fmKey;
+    if (id.fileMoveId) {
+      fmKey = datastore.key([this.kind, datastore.int(id.fileMoveId)]);
+    } else {
+      fmKey = datastore.key([this.kind]);
+    }
+    return dutil.concat([userKey, fmKey]);
+  }
+
+  idFromKey(k: DatastoreKey): FileMoveKey|undefined {
+    if (! k) {
+      throw new Error('Cannot generate a FileMoveKey from null key');
+    }
+    if (! k.parent) {
+      throw new Error('Cannot generate a FileMoveKey from a key with no parent ' + k);
+    }
+    let ret = new FileMoveKey();
+    ret.ownerId = k.parent.id || k.parent.name;
+    ret.fileMoveId = k.id;
+    return ret;
+  }
+
+  async save(fileMoves: FileMove[]): Promise<(FileMoveKey|undefined)[]> {
+    let error = this.validate(fileMoves);
+    if (error) {
+      throw error;
+    }
+    let entities = fileMoves.map(t => this.toEntity(t));
+    let savedEntities = await datastore.save(entities);
+    let mutationResults = savedEntities[0].mutationResults as any[];
+    // mr.key is only set (i.e. non-empty) if datastore generated a key for
+    // us. If we specify a key/id ahead of time, then it's empty.
+    return mutationResults.map(mr => {
+      if (! mr.key) {
+        return undefined;
+      }
+      let rk = new FileMoveKey();
+      rk.ownerId = mr.key.path[0].id || mr.key.path[0].name;
+      rk.fileMoveId = mr.key.path[1].id || mr.key.path[1].name;
+      return rk;
+    });
+  }
+
+  async findByOwner(ownerId: string): Promise<FileMove[]> {
+    let q = this.all()
+      .hasAncestor(UserService.keyFromId(ownerId))
+      .order('when', {descending: true});
+    return await this.query(q);
+  }
+}
+
+export let FileMoveService = new FileMoveSchema();
--- src/models/index.ts
+++ src/models/index.ts
@@ -7,3 +7,4 @@ export { DatastoreUtil } from './base';
 export { VisitService } from './visit';
 export { User, UserService } from './user';
 export { Rule, RuleService, RuleKey } from './rule';
+export { FileMove, FileMoveService, FileMoveKey } from './filemove';
