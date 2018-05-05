fc17cafcd7a4070164ace95cebe95a14b1200ccb
Mustafa Paksoy <paksoy@gmail.com>
2018-05-05 14:47:19 -0700

    Start scaffolding user processing in webhook

--- src/routes/dropboxWebhook.ts
+++ src/routes/dropboxWebhook.ts
@@ -13,7 +13,11 @@ app.get('/dropbox/webhook', function(req, res) {
   res.send(req.query.challenge || 'OK');
 });
 
-import {} from '../redis';
+import {
+  shouldSortUser,
+  startQueueProcessor,
+  onUserReadyForSort,
+} from '../redis';
 import crypto = require('crypto');
 
 app.post('/dropbox/webhook', function(req, res, next) {
@@ -34,15 +38,18 @@ app.post('/dropbox/webhook', function(req, res, next) {
       next(err);
       return;
     }
+    // Now we know that the signature is good, let's process users
     // We respond right away to keep Dropbox happy, we're not really done
     // procesing the request
     res.send('OK');
 
     const json = JSON.parse(body);
-    console.log('Good sig!', json);
     let ids: string[] = json.list_folder.accounts;
-    console.log('ids', ids);
     for (let id of ids) {
+      if (! await shouldSortUser(id)) {
+        console.log(`Not processing users ${id}, throttled.`);
+        return;
+      }
       let user = await UserService.findByDropboxId(id);
       if (user) {
         let dbx = dropbox(user.dropboxV2Token);
@@ -55,13 +62,20 @@ app.post('/dropbox/webhook', function(req, res, next) {
         } else {
           res = await dbx.client.filesListFolder({ path: user.sortingFolder as string });
         }
+        let entries: any[] = res.entries.filter((entry: any) => entry['.tag'] === 'file');
+        console.log('New files to process for user', id, entries);
+
+        // TODO process user
         user.dropboxCursor = res.cursor;
         await UserService.save([user]);
-        let entries: any[] = res.entries.filter((entry: any) => entry['.tag'] === 'file');
-        console.log(id, entries);
       }
     }
   });
+});
+
+
+onUserReadyForSort(async function(userId) {
+  // TODO process user
 });
 
 export default app;
