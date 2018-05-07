828fa3bdc671ba31d3d612c1e891480ccfb71481
Mustafa Paksoy <paksoy@gmail.com>
2018-05-07 16:03:37 -0700

    Don't call next() in webhook, always respond with 200

--- src/routes/dropboxWebhook.ts
+++ src/routes/dropboxWebhook.ts
@@ -28,45 +28,41 @@ app.post('/dropbox/webhook', function(req, res, next) {
     chunks.push(chunk as Buffer);
   });
   req.on('end', reportError(async function() {
-    try {
-      const body = Buffer.concat(chunks).toString('utf8');
-      let hmac = crypto.createHmac('sha256', process.env.DROPBOX_SECRET as string);
-      hmac.update(body);
-      let expectedSig = hmac.digest('hex');
-      let sig = req.header('X-Dropbox-Signature');
-      if (expectedSig !== sig) {
-        res.status(401).send('Bad signature');
-        let err: any = new Error('Bad signature');
-        err.status = 401;
-        next(err);
+    const body = Buffer.concat(chunks).toString('utf8');
+    let hmac = crypto.createHmac('sha256', process.env.DROPBOX_SECRET as string);
+    hmac.update(body);
+    let expectedSig = hmac.digest('hex');
+    let sig = req.header('X-Dropbox-Signature');
+    if (expectedSig !== sig) {
+      res.status(401).send('Bad signature');
+      let err: any = new Error('Bad signature');
+      err.status = 401;
+      next(err);
+      return;
+    }
+    // Now we know that the signature is good, let's process users
+    // We respond right away to keep Dropbox happy, we're not really done
+    // procesing the request
+    res.send('OK');
+
+    const json = JSON.parse(body);
+    let ids: string[] = json.list_folder.accounts;
+    for (let id of ids) {
+      if (! await shouldSortUser(id)) {
+        console.log(`Not processing users ${id}, throttled.`);
         return;
       }
-      // Now we know that the signature is good, let's process users
-      // We respond right away to keep Dropbox happy, we're not really done
-      // procesing the request
-      res.send('OK');
-
-      const json = JSON.parse(body);
-      let ids: string[] = json.list_folder.accounts;
-      for (let id of ids) {
-        if (! await shouldSortUser(id)) {
-          console.log(`Not processing users ${id}, throttled.`);
-          return;
-        }
-        let user = await UserService.findByDropboxId(id);
-        if (user) {
-          if (user.periodicSort) {
-            let dbx = dropbox(user.dropboxV2Token);
-            await dbx.runRulesAndUpdateUserAndFileMoves(user, true);
-          } else {
-            console.error(`Not sorting user, periodic sort disabled: ${id}`);
-          }
+      let user = await UserService.findByDropboxId(id);
+      if (user) {
+        if (user.periodicSort) {
+          let dbx = dropbox(user.dropboxV2Token);
+          await dbx.runRulesAndUpdateUserAndFileMoves(user, true);
         } else {
-          console.error(`Attempting to sort user that is not in our Database: ${id}`);
+          console.error(`Not sorting user, periodic sort disabled: ${id}`);
         }
+      } else {
+        console.error(`Attempting to sort user that is not in our Database: ${id}`);
       }
-    } catch (e) {
-      next(e);
     }
   }));
 });
