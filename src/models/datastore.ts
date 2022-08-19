8a4a4bc785d16ac765cbea29dd67b4d79efacadb
Mustafa Paksoy <paksoy@gmail.com>
2022-08-19 14:08:52 -0700

    wip

--- src/models/datastore.ts
+++ src/models/datastore.ts
@@ -1,3 +1,3 @@
-import Datastore = require('@google-cloud/datastore');
+import { Datastore } from "@google-cloud/datastore";
 const datastore = new Datastore({});
 export default datastore;
--- src/test/dropbox.test.ts
+++ src/test/dropbox.test.ts
@@ -1,14 +1,12 @@
-import { RuleService as rs, Rule } from '../models';
-import dropbox, { MoveResult } from '../dropbox';
-import _ = require('underscore');
-import { assert } from 'chai';
+import { RuleService as rs, Rule } from "../models";
+import dropbox, { MoveResult } from "../dropbox";
+import _ = require("underscore");
+import { assert } from "chai";
 
-const {
-  DROPBOX_TEST_TOKEN,
- } = process.env;
+const { DROPBOX_TEST_TOKEN } = process.env;
 
-const testingFolder = '/testing';
-const protoFolder = '/proto testing';
+const testingFolder = "/testing";
+const protoFolder = "/proto testing";
 
 const rule1Files = [
   "testfoo.txt",
@@ -18,13 +16,12 @@ const rule1Files = [
   "testfoo (4).txt",
   "testfoo (5).txt",
 ];
-const rule2Files = [
-  "moose.json",
-  "moose (2).json",
-  "peach.json",
-];
+const rule2Files = ["moose.json", "moose (2).json", "peach.json"];
 
-describe("Dropbox", function() {
+describe("Dropbox", function () {
+  if (!DROPBOX_TEST_TOKEN) {
+    assert.fail("Missing env variable DROPBOX_TEST_TOKEN");
+  }
   const dbx = dropbox(DROPBOX_TEST_TOKEN);
 
   let uniqueId: string;
@@ -33,30 +30,30 @@ describe("Dropbox", function() {
 
   function rules(): Rule[] {
     let ret: Rule[] = [];
-    let rule = rs.makeNew('user' + uniqueId);
+    let rule = rs.makeNew("user" + uniqueId);
     // This first rule should be skipped since the destination is the sorting
     // folder
-    rule.type = 'EXT_EQ';
-    rule.pattern = 'json';
+    rule.type = "EXT_EQ";
+    rule.pattern = "json";
     rule.dest = testSortingFolder; // << not allowed!
     ret.push(rule);
-    rule = rs.makeNew('user' + uniqueId);
-    rule.type = 'NAME_CONTAINS';
-    rule.pattern = 'testf';
-    rule.dest = testFolder + '/rule1';
+    rule = rs.makeNew("user" + uniqueId);
+    rule.type = "NAME_CONTAINS";
+    rule.pattern = "testf";
+    rule.dest = testFolder + "/rule1";
     ret.push(rule);
-    rule = rs.makeNew('user' + uniqueId);
-    rule.type = 'EXT_EQ';
-    rule.pattern = 'json';
-    rule.dest = testFolder + '/rule2/';
+    rule = rs.makeNew("user" + uniqueId);
+    rule.type = "EXT_EQ";
+    rule.pattern = "json";
+    rule.dest = testFolder + "/rule2/";
     ret.push(rule);
     return ret;
   }
 
-  beforeEach(async function() {
-    uniqueId = String(Math.random()).split('.')[1];
+  beforeEach(async function () {
+    uniqueId = String(Math.random()).split(".")[1];
     testFolder = testingFolder + `/t${uniqueId}`;
-    testSortingFolder = testFolder + '/SortMyBox';
+    testSortingFolder = testFolder + "/SortMyBox";
 
     await dbx.client.filesCopyV2({
       from_path: protoFolder,
@@ -64,31 +61,31 @@ describe("Dropbox", function() {
     });
   });
 
-  afterEach(async function() {
+  afterEach(async function () {
     await dbx.client.filesDeleteV2({ path: testFolder });
   });
 
-  it("End to end test", async function() {
+  it("End to end test", async function () {
     let moveResult = await dbx.runRules(testSortingFolder, rules());
     let expected: MoveResult[] = [];
-    rule1Files.forEach(f => {
+    rule1Files.forEach((f) => {
       expected.push({
         fileName: f,
-        fullDestPath: testFolder + '/rule1/' + f,
+        fullDestPath: testFolder + "/rule1/" + f,
         conflict: false,
       });
     });
 
-    rule2Files.forEach(f => {
+    rule2Files.forEach((f) => {
       expected.push({
         fileName: f,
-        fullDestPath: testFolder + '/rule2/' + f,
+        fullDestPath: testFolder + "/rule2/" + f,
         conflict: false,
       });
     });
 
-    let moves = _.sortBy(moveResult.results, 'fullDestPath');
-    expected = _.sortBy(expected, 'fullDestPath');
+    let moves = _.sortBy(moveResult.results, "fullDestPath");
+    expected = _.sortBy(expected, "fullDestPath");
     assert.deepEqual(moves, expected);
   });
 });
